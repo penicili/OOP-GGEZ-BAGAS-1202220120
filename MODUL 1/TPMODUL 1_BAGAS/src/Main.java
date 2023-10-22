d56dbe287c2b17bb09e0ab42561edef4b226edf2
Katon Bagaskoro <bagaskoro55@gmail.com>
2023-10-22 22:19:17 +0700

    Mengumpulkan tugas

--- MODUL 1/TPMODUL 1_BAGAS/src/Main.java
+++ MODUL 1/TPMODUL 1_BAGAS/src/Main.java
@@ -1,19 +1,5 @@
 public class Main {
-    public static void main(String[] args){
-        Bank bank = new Bank();
-        Account Bagas = new Account("Bagas_SI4606", "1202220120", 100000);
-        bank.addAccount(Bagas);
-
-        Account Lail = new Account("Lail_SI4606", "1202220121",200000);
-        bank.addAccount(Lail);
-
-        Account Esok = new Account("Esok_SI4606", "1202220122",300000 );
-        bank.addAccount(Esok);
-
-        Bagas.deposit(50000);
-        Esok.withdraw(120000);
-
-        bank.removeAccount("Lail");
-        bank.getAllAccounts();
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
     }
 }
